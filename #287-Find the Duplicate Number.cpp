1adbaf8a23a79780d8cc6ab17657f78c13d9f0e0
Bhanu <aggarwal.bhanu02@gmail.com>
2020-07-21 21:15:35 +0530

    Add files via upload

---
+++ #287-Find the Duplicate Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int> m;
+        int result;
+        for(int i=0;i<nums.size();i++){
+            if(m[nums[i]])
+                m[nums[i]]++;
+            else
+                m[nums[i]]=1;
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(m[nums[i]]>=2){
+                result=nums[i];
+            }
+        }
+        return result;
+    }
+};
