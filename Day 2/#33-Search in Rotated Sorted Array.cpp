93b8ed5d5e1220c096d1a12fab9ba3ac839cbf5b
Bhanu <aggarwal.bhanu02@gmail.com>
2020-07-23 02:36:19 +0530

    Add files via upload

---
+++ Day 2/#33-Search in Rotated Sorted Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+    
+        if(find(nums.begin(),nums.end(),target)!=nums.end())
+            return find(nums.begin(),nums.end(),target)-nums.begin();
+        return -1;
+    }
+  
+};
